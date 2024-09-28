0539ed42458e78eef8d780f507fc8d419ee33d32
jimingu <jimingu0531@gmail.com>
2024-09-29 03:38:53 +0900

    곱셈을 덧셈으로 변경

--- Example1.py
+++ Example1.py
@@ -2,4 +2,4 @@ x = int(input('첫 번째 피연산자를 입력하세요 : '))
 y = int(input('두 번째 피연산자를 입력하세요 : '))
 z = int(input('세 번째 피연산자를 입력하세요 : '))
 
-print(f'{x} x {y} x {z} = {x*y*z}')
+print(f'{x} + {y} + {z} = {x+y+z}')
